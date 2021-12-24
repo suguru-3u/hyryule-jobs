af82ae6b51bfebf8c33a9a7212f4e3af142dc1d4
suguru-3u <suguru.13579515@gmal.com>
2021-12-24 13:18:57 +0900

    Study TypeScript

---
+++ src/components/Joblist.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="job-list">
+        <p>Order by {{ order }}</p>
+        <transition-group name="list" tag="ul">
+            <li v-for=" job in orderJobs" :key="job.id">
+                <h2>{{ job.title }} in {{ job.location }}</h2>
+                <div class="salary">
+                    <p>{{ job.salary }} rupeses</p>
+                </div>
+                <div class="description">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem omnis voluptatum eius doloremque optio iusto sequi dignissimos. Pariatur earum assumenda dolores possimus quidem quam, reprehenderit aliquid consequuntur amet non facere.</p>
+                </div>
+            </li>
+        </transition-group>
+    </div>
+</template>
+
+<script lang="ts">
+import OrderTerm from '@/type/OrderTerm'
+import { computed, defineComponent,PropType } from 'vue'
+import Job from '../type/job'
+
+export default defineComponent({
+    props: {
+        jobs: {
+            type: Array as PropType<Job[]>,
+            required: true
+        },
+        order: {
+            type: String as PropType<OrderTerm>,
+            required: true
+        }
+    },
+    setup(props) {
+        const orderJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ?  1 : -1
+            })
+        })
+
+        return { orderJobs }
+    },
+})
+</script>
+
+<style scoped>
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+   .list-move {
+    transition: all 1s;
+  }
+</style>
+
